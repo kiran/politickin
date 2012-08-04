1f3425cc989f4519662d47eb86fa4711e65b9835
kiran-b <kiranb@mit.edu>
2012-08-04 01:55:04 -0700

    bootstrapped!

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,5 @@
+class LandingController < ApplicationController
+	layout 'landing'
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Politickin::Application.routes.draw do
+  root :to => "landing#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
