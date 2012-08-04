f54d8b8aa1b553a5a8340c46b02128f1125478e5
kiran-b <kiranb@mit.edu>
2012-08-03 23:10:44 -0700

    set up rails app.  nothing in it.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politickin::Application.config.secret_token = '0bd7655e6072e41de88da38fbbda4a3fe1ddea28bcd0fe6dab7eca453d0ddde435865f36daba72a47b29318469dc5b237a5b7cbc98325b06492455d73c4f824d'
