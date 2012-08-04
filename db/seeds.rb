9b72139f87190bd1256d0b66dc04f4dd18b100f3
Prateek Maheshwari <prateekm@prateekm.linkedin.biz>
2012-08-04 07:22:08 -0700

    db seeded!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Congressman.delete_all
+open("db/seeds/final_joined_tables.tsv") do |groups|
+    groups.read.each_line do |group|
+        next if group.empty?
+        name,party,title,constituency,json_stances = group.chomp.split("\t")
+        Congressman.create!(:name=>name, :party=>party, :title=>title, :constituency=>constituency, :json_stances=>json_stances)
+    end
+end
